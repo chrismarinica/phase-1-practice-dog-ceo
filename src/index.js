2af6a80e9c50f2b7c2b28dcf87b2aee1691f3bb4
Christopher Marinica <chris.marinica@gmail.com>
2022-11-04 10:48:24 -0400

    commit tree 18354fbf098d2d3eb2bcfddf682f96ca096b20e4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((resp) => resp.json())
+    .then((img) => renderBooks(img));
+  }
+
+  function renderImages(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const img = document.createElement('img');
+      h2.innerHTML = book.name;
+      main.appendChild(img);
+    });
+  }
